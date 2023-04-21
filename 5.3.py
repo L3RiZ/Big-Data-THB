15294c09b6cbe1db295b58cceaddbe79826f6eaf
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-04-21 15:15:45 +0200

    struggle

--- 5.3.py
+++ 5.3.py
@@ -1,16 +1,33 @@
 class RomNumber: 
-    def printRoman(number):
-        num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
-        sym = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
-        i = 12
+    #class variable
+    roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
 
-        while number:
-            div = number // num[i]
-            number %= num[i]
+    def print_roman(number):
+        final_str = ""
+
+        for j in range(len(RomNumber.roman)-1, -1, -1):
+            output = number // RomNumber.numbers[j]
+            number %=  RomNumber.numbers[j]
+            while output: 
+                final_str += RomNumber.roman[j]
+                output -= 1
+            
+        return final_str
     
-            while div:
-                print(sym[i], end = "")
-                div -= 1
-            i -= 1
+    def print_number(number):
+        final_int = 0
 
-print(RomNumber.printRoman(21))
+        for j in range(len(number)):
+            if RomNumber.numbers[RomNumber.roman.index(number[j])] < RomNumber.numbers[RomNumber.roman.index(number[j+1])]:          
+                final_int += RomNumber.numbers[RomNumber.roman.index(number[j] + number[j+1])]  
+                j += 1            
+                print(final_int) 
+            else:
+                final_int += RomNumber.numbers[RomNumber.roman.index(number[j])] 
+                print(final_int) 
+
+        return final_int
+                
+
+print(RomNumber.print_number("MCMXCIX"))
